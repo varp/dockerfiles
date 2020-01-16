1305465b04c0f11469379c833e0d7bb3bae42d11
Vardan Pogosian <vardan.pogosyan@gmail.com>
2020-01-16 17:45:27 +0300

    feat(ubuntu): add docker-entrypoint.sh for php-fpm image

---
+++ ubuntu/php/docker-entrypoint.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+set -Eeo pipefail
+
+_set_debug_host() {
+  if ! ping -c1 -q docker.for.mac.host.internal >/dev/null 2>&1; then
+    local ip=$(ping -c1 -q docker.for.mac.host.internal | tr -d '():' | awk '{print $3}')
+  else
+    local ip=$(ip route list default | awk '{print $3}')
+  fi
+  echo "$ip docker.host.internal" | sudo tee -a /etc/hosts
+}
+
+[[ -z "$(grep 'docker.host.internal' /etc/hosts)" ]] && _set_debug_host
+
+# if first arg looks like a flag, assume we want to run postgres server
+if [ "${1:0:1}" = '-' ]; then
+  set -- sudo /usr/sbin/php-fpm "$@"
+else
+  exec "$@"
+fi
+
+if [ $# -eq 0 ]; then
+  exec sudo /usr/sbin/php-fpm
+fi
