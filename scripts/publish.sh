823bcdf9a225d648b310a5d4e32da6844cdb9904
Vardan Pogosian <vardan.pogosyan@gmail.com>
2020-09-03 14:32:52 +0300

    fix(script): refactored publish.sh

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -2,34 +2,33 @@
 
 set -e
 
-## .env must containt 
-# DOCKER_REGISTRY=docker.pkg.github.com
-# DOCKER_REPOSITORY=varp/dockerfiles
-# DOCKER_USERNAME=varp
-# DOCKER_PASSWORD=GITHUB_TOKEN
-
-[ ! -f .env ] && {
-  echo "[-] .env was not found. aborting"
-  exit 1
-}
-
-. .env
 
 SKIP_BUILD="$1"
 USE_CUSTOM_REPO="$2"
 
 DEFAULT_REGISTRY="vardan"
 
-
-# ubuntu:base ubuntu:basecli ubuntu:php ubuntu:php-7.2 ubuntu:php-7.3 ubuntu:php-7.4
 IMAGES=( centos:base centos:basecli ubuntu:base ubuntu:basecli ubuntu:php-7.2 ubuntu:php-7.3 ubuntu:php-7.4 )
 [ ! -z "$USE_CUSTOM_REPO" ] && {
-  echo "+ log in to ${DOCKER_REGISTRY}" 
+
+  [ ! -f .env ] && {
+    echo "[-] .env was not found. aborting"
+    exit 1
+  }
+
+  ## .env must containts
+  # DOCKER_REGISTRY=docker.pkg.github.com
+  # DOCKER_REPOSITORY=varp/dockerfiles
+  # DOCKER_USERNAME=varp
+  # DOCKER_PASSWORD=GITHUB_TOKEN
+  . .env
+
+  echo "[+] log in to ${DOCKER_REGISTRY}" 
   docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD $DOCKER_REGISTRY
 }
 
 [ -z "$SKIP_BUILD" ] && { 
-  echo "+ building images"
+  echo "[+] building images"
 
   docker-compose build
 }
@@ -38,7 +37,7 @@ NEW_TAGS=( )
 DATE=$(date +"%Y%m%d")
 
 
-echo "+ assigning tags"
+echo "[+] assigning tags"
 for image in "${IMAGES[@]}"; do
   
   if [ ! -z "$USE_CUSTOM_REPO" ]; then
@@ -54,7 +53,7 @@ for image in "${IMAGES[@]}"; do
 
 done
 
-echo "+ pushing images"
+echo "[+] pushing images"
 for tag in "${NEW_TAGS[@]}"; do
   
   docker push "$tag"
